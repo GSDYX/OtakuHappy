1d7d932a1f88bd6ec4d8da1b97fcb4d005934ef5
yournameGSDYX <564669854@qq.com>
2018-05-29 22:01:27 +0800

    time line

--- pages/cartoon/cartoon.js
+++ pages/cartoon/cartoon.js
@@ -1,65 +1,77 @@
+
+var time = 0;
+var touchDot = 0;//触摸时的原点
+var interval = "";
+var flag_hd = true;
+
+
+var animation = wx.createAnimation({
+  duration: 500,
+  timingFunction: 'ease',
+});
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    
+    data: {
+    n: 1,
+    animationData: {}
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    
+  onLoad: function () {
+   
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
+  
     
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
+  // 触摸开始事件
+  touchStart: function (e) {
+    touchDot = e.touches[0].pageX; // 获取触摸时的原点
+    // 使用js计时器记录时间    
+    interval = setInterval(function () {
+      time++;
+    }, 100);
   },
+  // 触摸结束事件
+  touchEnd: function (e) {
+    var touchMove = e.changedTouches[0].pageX;
+    // 向左滑动   
+    if (touchMove - touchDot <= -40 && time < 10 && flag_hd == true) {
+      flag_hd = false;
+      //执行切换页面的方法
+      console.log("向右滑动");
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
+  
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
+    this.animation = animation;
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
+    animation.rotateY(180).step();
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+    this.setData({
+
+      animationData: animation.export()
+
+    })
+
+    }
+    // 向右滑动   
+    if (touchMove - touchDot >= 40 && time < 10 && flag_hd == true) {
+      flag_hd = false;
+      //执行切换页面的方法
+      console.log("向左滑动");
+
+
+      this.animation = animation;
+
+      animation.rotateY(-180).step();
+
+      this.setData({
+
+        animationData: animation.export()
+
+      })
+    }
+    clearInterval(interval); // 清除setInterval
+    time = 0;
+    flag_hd = true;  
   }
 })
